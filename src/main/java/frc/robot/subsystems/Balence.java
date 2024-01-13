fa516816da32a8767b3b208423620cd315a98430
George Peterson <george.d.peterson@gmail.com>
2024-01-13 10:12:58 -0500

    Initial 2024 Fire Code

---
+++ src/main/java/frc/robot/subsystems/Balence.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+//import com.kauailabs.navx.frc.AHRS;
+
+
+public class Balence{ 
+    //AHRS ahrs;
+
+    //public double getTilt(){
+        //double tilt = ahrs.getRoll();
+        //double currentTilt = tilt;
+        //return currentTilt;
+    //}
+}
