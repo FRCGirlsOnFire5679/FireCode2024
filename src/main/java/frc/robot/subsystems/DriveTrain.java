faa50f8a8594c44b125c8354e5fdd5e5884738da
camsm <camsmith13sandy@gmail.com>
2024-01-18 11:57:17 -0500

    fixed trigger code

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -97,8 +97,8 @@ public class DriveTrain extends SubsystemBase {
     }
 
     public void crawl(double left, double right, double pressed){
-        double crawlFactor = (pressed - 0.5) * 2;
-        //double crawlFactor = pressed;
+        //double crawlFactor = (pressed - 0.5) * 2;
+        double crawlFactor = pressed;
         robotDrive.tankDrive(left * (Constants.DriveConstants.kCrawlLeftSpeedFactor * crawlFactor), right * (Constants.DriveConstants.kCrawlRightSpeedFactor * crawlFactor));
     }
 }
